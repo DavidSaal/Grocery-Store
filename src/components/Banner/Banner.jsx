feebc0323bfd76b2bcd1cc64a8a72ce0417a9f32
David Saal <daviid.saal@gmail.com>
2022-03-06 00:49:00 +0200

    fix banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,20 +4,18 @@ import "./Banner.css";
 
 const Banner = ({ setSearchedValue }) => {
   return (
-    <div className="banner-search">
-      <form className="d-flex row input-group text-center justify-content-center align-items-center">
-        <h1 className="pb-2">Products Delivered in 90 Minutes</h1>
-        <p className="pb-2">
-          Get your products delivered at your doorsteps all day everyday
-        </p>
-        <div className="w-40">
-          <Search
-            setSearchedValue={setSearchedValue}
-            placeholder="E.g: Meat, Yogurt, Eggs&#8230;"
-          />
-        </div>
-      </form>
-    </div>
+    <form className="d-flex row input-group text-center justify-content-center align-items-center">
+      <h1 className="pb-2">Products Delivered in 90 Minutes</h1>
+      <p className="pb-2">
+        Get your products delivered at your doorsteps all day everyday
+      </p>
+      <div className="w-40">
+        <Search
+          setSearchedValue={setSearchedValue}
+          placeholder="E.g: Meat, Yogurt, Eggs&#8230;"
+        />
+      </div>
+    </form>
   );
 };
 
